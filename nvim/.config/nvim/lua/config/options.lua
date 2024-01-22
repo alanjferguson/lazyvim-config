5dba00a927aae0fd1202222d03d8ebc242829956
Alan J. Ferguson <alan@ferguson.info>
2024-01-22 14:47:09 +0000

    Disable list and autoformat

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Better whitespace chars
+vim.opt.list = false
+vim.opt.listchars = {
+  tab = "> ",
+  trail = " ",
+  nbsp = "+",
+}
+
+-- Disable Autoformat everywhere
+vim.g.autoformat = false

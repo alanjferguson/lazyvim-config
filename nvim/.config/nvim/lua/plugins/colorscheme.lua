3c514607a6353860961943ac5281f926efd5a843
Alan J. Ferguson <alan@ferguson.info>
2025-01-12 14:45:09 +0000

    Fix colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,8 @@ return {
   {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
-    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('github-theme').setup({
         -- ...
@@ -13,17 +13,19 @@ return {
     end,
   },
   {
-  "f-person/auto-dark-mode.nvim",
-  opts = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("colorscheme github_dark_default")
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("colorscheme github_light_default")
-    end,
-  },
+    "f-person/auto-dark-mode.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 999, -- make sure to load this before all the other start plugins
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme github_dark_default")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme github_light_default")
+      end,
+    },
   }
 }
